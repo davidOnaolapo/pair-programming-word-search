37ceefc75a0e073afad72f5fa02bc49a86639701
David Onaolapo <talk2dawich@gmail.com>
2021-06-30 21:26:46 +0000

    Passed all tests, word search works

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,26 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is vertical", function() {
+    const result = wordSearch([
+      ['M', 'E', 'U', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MAY')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if array is empty", function() {
+    const result = wordSearch([[]], 'MAY')
+
+    assert.isFalse(result);
+  });
 });
